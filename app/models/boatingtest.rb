a356e98f6e702406912eb429128321db46dabece
Agandaur <cjwagstaffe@gmail.com>
2020-02-03 17:43:09 -0800

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,21 @@
 class BoatingTest
+    attr_accessor :test_name, :test_status
+    attr_reader :student, :instructor
+    @@all = []
 
+    def initialize(student, test_name, test_status, instructor)
+        @student = student
+        @test_name = test_name
+        @test_status = test_status
+        @instructor = instructor
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
 end
